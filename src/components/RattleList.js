10cc1c3520e3951cf686630fdad05e52d5c5076a
Melissa Guachun <1212malka@gmail.com>
2021-09-15 14:55:03 -0400

    rattle list

---
+++ src/components/RattleList.js
@@ -0,0 +1,28 @@
+import Rattle from "./Rattle";
+import "../assets/styles/list.css";
+
+const RattleList = ({ rattles }) => {
+  // const rattlesReversed = rattles.reverse();
+  const renderFweets = () => {
+    if (rattles && rattles.length !== 0) {
+      return rattles.map((fweet) => (
+        <Rattle rattleObj={rattle} key={rattle.username} />
+      ));
+    } else {
+      return <p className="sorry">Sorry, there are no death rattles yet...</p>;
+    }
+  };
+
+  return (
+    <div className="list">
+      <h1>
+        RattleFeed &nbsp;
+        <i className="fas fa-dove"></i>
+      </h1>
+
+      {renderRattles()}
+    </div>
+  );
+};
+
+export default RattleList;
