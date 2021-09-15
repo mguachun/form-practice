dfa8e03f36902862eaff66a94166047e24cc09f0
Melissa Guachun <1212malka@gmail.com>
2021-09-15 14:51:06 -0400

    set up files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import ControlledForm from "./components/forms/ControlledForm";
+import ControlledValidation from "./components/forms/ControlledValidation";
+import RattleList from "./components/RattleList";
+import RattleForm from "./components/RattleForm";
+import "./assets/styles/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  //create some initial state 
+  state = {
+    rattles: [
+      {
+      username: "EdgarAPoe",
+      rattle: "@VirginiaPoe u up?",
+      },
+      {
+        username: "VirginiaPoe",
+        rattle: "@EdgarAPoe 	:sleeping:"
+      },
+    ],
+  };
+
+  //function to be added as a callback prop
+
+  addRattle = (newRattle) => {
+    this.setState((prevState) => {
+      return {
+        rattles: [...prevState.rattles, newRattle] //spread operator
+      };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <Header />
+        </div>
+
+        <div className="container">
+
+          {/* parent */}
+          <div className="rattle-list">
+
+            {/* passing rattles in as props */}
+            <RattleList rattles={this.state.rattles} />
+
+          </div>
+
+          <div className="rattle-form">
+
+            {/* passing in addRattle as a callback function  */}
+            <RattleForm addRattle={this.addRattle} />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
