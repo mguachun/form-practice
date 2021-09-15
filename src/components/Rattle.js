9e5c178e94defa6bdaf5e557d815e2f30d4381c4
Melissa Guachun <1212malka@gmail.com>
2021-09-15 14:51:30 -0400

    rattle post

---
+++ src/components/Rattle.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import ".../assets/styles/rattle.css";
+
+class Rattle extends React.Component {
+    state = {
+        likes: 0,
+    };
+
+    handleClick = (e) => {
+        e.target.classList.remove("far");
+        e.target.classList.add("fas", "full");
+
+        this.setState((prevState)=> {
+            return {
+                likes: prevState.likes + 1,
+            };
+        });
+    };
+
+    render() {
+        const { username, rattle } = this.props.rattleObj;
+        return (
+            <div className="rattle-box">
+                <p className="rattle-content">{rattle}</p>
+                <div className="footer">
+                    <p>
+                        {this.state.likes} {" "}
+                        <i className="far fa-heart" onClick={this.handleClick}></i>
+                    </p>
+                    <p>@{username}</p>
+                </div>
+            </div>
+        );
+    }
+ }
+export default Rattle;
